bcd145d7c736782d92b1498403e39eb3e9f20d4b
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2015-04-02 14:00:41 +0200

    Adding tools reference

--- rtlsdr-generate-wideband.sh
+++ rtlsdr-generate-wideband.sh
@@ -4,4 +4,4 @@ SERVER=hv01
 SDR=rtlsdr
 NODE_ID=0
 
-sudo ~/$SDR/heatmap.py /mnt/$SERVER/$SDR/current/data/node-$NODE_ID.csv /mnt/$SERVER/$SDR/current/node-$NODE_ID.png
+sudo ~/$SDR/tools/heatmap.py /mnt/$SERVER/$SDR/current/data/node-$NODE_ID.csv /mnt/$SERVER/$SDR/current/node-$NODE_ID.png
