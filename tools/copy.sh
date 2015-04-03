471760f22817a6865924d6715d64e55dddce14b8
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2015-04-03 22:04:14 +0200

    Fixing generate and adding copy-script to tools directory

---
+++ tools/copy.sh
@@ -0,0 +1,3 @@
+mv /mnt/hv01/rtlsdr/current/node-0.bmp /mnt/hv01/rtlsdr/current/node-0_`date +%Y-%m-%d_%H-%M`.bmp
+mv /mnt/hv01/rtlsdr/current/node-1.bmp /mnt/hv01/rtlsdr/current/node-1_`date +%Y-%m-%d_%H-%M`.bmp
+
