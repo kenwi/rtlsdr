4b0bfc4cd496b823d260ed158d7d9ccd40c0e86a
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2015-04-03 22:08:08 +0200

    Changed filename formatting

--- tools/copy.sh
+++ tools/copy.sh
@@ -1,3 +1,3 @@
-mv /mnt/hv01/rtlsdr/current/node-0.bmp /mnt/hv01/rtlsdr/current/node-0_`date +%Y-%m-%d_%H-%M`.bmp
-mv /mnt/hv01/rtlsdr/current/node-1.bmp /mnt/hv01/rtlsdr/current/node-1_`date +%Y-%m-%d_%H-%M`.bmp
+mv /mnt/hv01/rtlsdr/current/node-0.bmp /mnt/hv01/rtlsdr/current/`date +%Y%m%d_%H%M_node-0`.bmp
+mv /mnt/hv01/rtlsdr/current/node-1.bmp /mnt/hv01/rtlsdr/current/`date +%Y%m%d_%H%M_node-1`.bmp
 
