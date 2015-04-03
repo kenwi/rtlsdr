94c059dbd59ddcdf997df9847d3a4809cde4f616
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2015-04-03 06:32:59 +0200

    Fixing merges and shit

--- rtlsdr-generate-wideband.sh
+++ rtlsdr-generate-wideband.sh
@@ -12,7 +12,6 @@ echo "Writing file: $DST"
 sudo ~/$SDR/tools/heatmap.py $SRC $DST
 echo ""
 
-<<<<<<< HEAD
 NODE_ID=1
 SRC=$MNT/$SERVER/$SDR/current/data/node-$NODE_ID.csv
 DST=$MNT/$SERVER/$SDR/current/node-$NODE_ID.bmp
@@ -20,20 +19,3 @@ echo "Reading data: $SRC"
 echo "Writing file: $DST"
 sudo ~/$SDR/tools/heatmap.py $SRC $DST
 echo ""
-=======
-# Home
-SRC=$MNT/$SERVER/$SDR/current/data/node-$NODE_ID.csv
-DST=$MNT/$SERVER/$SDR/current/node-$NODE_ID.png
-echo "Reading data: $SRC"
-echo "Writing file: $DST"
-sudo ~/$SDR/tools/heatmap.py $SRC $DST
-
-NODE_ID=3
-SRC=$MNT/$SERVER/$SDR/current/data/node-$NODE_ID.csv
-DST=$MNT/$SERVER/$SDR/current/node-$NODE_ID.png
-echo "Reading data: $SRC"
-echo "Writing file: $DST"
-sudo ~/$SDR/tools/heatmap.py $SRC $DST
-
-
->>>>>>> 4168f704fac5d98e0f25b3d37fab189477c300a4
