df9f713918d1e7aa47b387063f7e412b5b1cbdb4
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2015-04-03 22:12:29 +0200

    generate-wideband should be runnable

--- rtlsdr-generate-wideband.sh
+++ rtlsdr-generate-wideband.sh
@@ -9,13 +9,16 @@ SRC=$MNT/$SERVER/$SDR/current/data/node-$NODE_ID.csv
 DST=$MNT/$SERVER/$SDR/current/node-$NODE_ID.bmp
 echo "Reading data: $SRC"
 echo "Writing file: $DST"
-sudo ~/$SDR/tools/heatmap.py $SRC $DST
-echo ""
+sh /home/kenwi/$SDR/tools/heatmap.py $SRC $DST &
 
 NODE_ID=1
 SRC=$MNT/$SERVER/$SDR/current/data/node-$NODE_ID.csv
 DST=$MNT/$SERVER/$SDR/current/node-$NODE_ID.bmp
 echo "Reading data: $SRC"
 echo "Writing file: $DST"
-sudo ~/$SDR/tools/heatmap.py $SRC $DST
-echo ""
+sh /home/kenwi/$SDR/tools/heatmap.py $SRC $DST &
+
+TIME=`date`
+echo "$TIME: Started generating images" >> /var/log/rtlsdr-generate-wideband.log
+
+
