4168f704fac5d98e0f25b3d37fab189477c300a4
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2015-04-03 06:24:19 +0200

    Script now kills existing rtl_power processes

--- rtlsdr-scan-wideband.sh
+++ rtlsdr-scan-wideband.sh
@@ -1,17 +1,27 @@
 #!/bin/sh
 
-DEVICE_ID=0
-
-START=30M
-STOP=1750M
-
-RESOLUTION=500k
 GAIN=50
 INTERVAL=10
 
-DESTINATION=/mnt/hv01/rtlsdr/current/data/node-0.csv
+sudo killall -9 rtl_power
 
-echo "Saving to $DESTINATION"
-echo ""
+# Device with antenna
+DEVICE_ID=0
+START=30M
+STOP=845M
+RESOLUTION=10k
+DESTINATION=/mnt/hv01/rtlsdr/current/data/node-$DEVICE_ID.csv
+echo "Saving from device:$DECIVE_ID to $DESTINATION"
+sudo rtl_power -i $INTERVAL -d $DEVICE_ID -f $START:$STOP:$RESOLUTION -g $GAIN $DESTINATION &
 
-sudo rtl_power -i $INTERVAL -d $DEVICE_ID -f $START:$STOP:$RESOLUTION -g $GAIN $DESTINATION
+sleep 2
+
+# Device 1
+DEVICE_ID=1
+START=845M
+STOP=1750M
+RESOLUTION=10k
+DESTINATION=/mnt/hv01/rtlsdr/current/data/node-$DEVICE_ID.csv
+echo "Saving from device:$DECIVE_ID to $DESTINATION"
+sudo rtl_power -i $INTERVAL -d $DEVICE_ID -f $START:$STOP:$RESOLUTION -g $GAIN $DESTINATION &
+
