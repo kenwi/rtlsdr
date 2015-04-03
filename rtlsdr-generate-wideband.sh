7f13fc46c54887bd180955f7377d5b72e19b0460
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2015-04-03 06:25:46 +0200

    Should be okay to cron now

--- rtlsdr-generate-wideband.sh
+++ rtlsdr-generate-wideband.sh
@@ -1,7 +1,21 @@
 #/bin/sh
 
+MNT=/mnt
 SERVER=hv01
 SDR=rtlsdr
-NODE_ID=0
 
-sudo ~/$SDR/tools/heatmap.py /mnt/$SERVER/$SDR/current/data/node-$NODE_ID.csv /mnt/$SERVER/$SDR/current/node-$NODE_ID.png
+NODE_ID=0
+SRC=$MNT/$SERVER/$SDR/current/data/node-$NODE_ID.csv
+DST=$MNT/$SERVER/$SDR/current/node-$NODE_ID.bmp
+echo "Reading data: $SRC"
+echo "Writing file: $DST"
+sudo ~/$SDR/tools/heatmap.py $SRC $DST
+echo ""
+
+NODE_ID=1
+SRC=$MNT/$SERVER/$SDR/current/data/node-$NODE_ID.csv
+DST=$MNT/$SERVER/$SDR/current/node-$NODE_ID.bmp
+echo "Reading data: $SRC"
+echo "Writing file: $DST"
+sudo ~/$SDR/tools/heatmap.py $SRC $DST
+echo ""
