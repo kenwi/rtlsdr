d5fa02077b56f76b4474ceca76c8695b48163038
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2015-04-03 22:22:08 +0200

    Added configuration for 3 nodes

--- tools/copy.sh
+++ tools/copy.sh
@@ -1,3 +1,4 @@
-mv /mnt/hv01/rtlsdr/current/node-0.bmp /mnt/hv01/rtlsdr/current/`date +%Y%m%d_%H%M_node-0`.bmp
+#mv /mnt/hv01/rtlsdr/current/node-0.bmp /mnt/hv01/rtlsdr/current/`date +%Y%m%d_%H%M_node-0`.bmp
 mv /mnt/hv01/rtlsdr/current/node-1.bmp /mnt/hv01/rtlsdr/current/`date +%Y%m%d_%H%M_node-1`.bmp
+#mv /mnt/hv01/rtlsdr/current/node-0.bmp /mnt/hv01/rtlsdr/current/`date +%Y%m%d_%H%M_node-2`.bmp
 
